980236bbd03c6dfef0eb5f9667639ce0cb4dc406
thaonguyen <thaonguyen1096@gmail.com>
2017-12-08 18:57:30 +0700

    proudct list

---
+++ SourceDoAn/SML_QLNPP/DataService/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Interfaces
+{
+    public interface IProductService
+    {
+        Product GetProduct(int idp);
+        List<Product> Search(string keyWord);
+    }
+}
---
+++ SourceDoAn/SML_QLNPP/DataService/Services/ProductService.cs
@@ -0,0 +1,51 @@
+﻿using DataModel;
+using DataModel.Interfaces;
+using DataService.Interfaces;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Services
+{
+    class ProductService: IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        ILogger logger = LogManager.GetCurrentClassLogger();
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Product GetProduct(int idp)
+        {
+            IRepository<Product> repository = _unitOfWork.Repository<Product>();
+            var result = repository.Get(a => a.IdProduct == idp);
+            return result != null ? result : null;
+        }
+
+        public List<Product> Search(string keyWord)
+        {
+            try
+            {
+                IRepository<Product> repository = _unitOfWork.Repository<Product>();
+                if (keyWord=="")
+                {
+                    return repository.GetAll().ToList();
+                }
+                else
+                {
+                    return repository.GetAll( a => (a.ProductName.Contains(keyWord) 
+                                                   || a.IdProduct.ToString().CompareTo(keyWord)==0)).ToList();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
