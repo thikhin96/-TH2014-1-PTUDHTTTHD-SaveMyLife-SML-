ee37a3608aa4e8b956015be757584e99e85e9ca3
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-12-16 16:12:42 +0700

    update log and

--- SourceDoAn/SML_QLNPP/DataService/Services/LogLoginService.cs
+++ SourceDoAn/SML_QLNPP/DataService/Services/LogLoginService.cs
@@ -14,23 +14,35 @@ namespace DataService
         private readonly IUnitOfWork _unitOfWork;
         private readonly IRepository<Log_Login> _LogLoginRepo;
         ILogger logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Hàm khởi tạo
+        /// </summary>
+        /// <param name="unitOfWork"></param>
+        /// <returns></returns>
         public LogLoginService(IUnitOfWork unitOfWork)
         {
             //_promotionRepository = unitOfWork.Repository<Promotion>();
             _unitOfWork = unitOfWork;
         }
-
+        /// <summary>
+        /// Thêm Log đăng nhập khi người dùng đăng nhập
+        /// </summary>
+        /// <param name="log"></param>
+        /// <returns></returns>
         public bool Add(Log_Login log)
         {
             IRepository<Log_Login> repository = _unitOfWork.Repository<Log_Login>();
             try
             {
+                logger.Info("Start audit login");
                 repository.Add(log);
                 _unitOfWork.SaveChange();
+                logger.Info("End audit login");
                 return true;
             }
-            catch
+            catch(Exception ex)
             {
+                logger.Info("Error audit login: " + ex.Message);
                 return false;
             }
         }
