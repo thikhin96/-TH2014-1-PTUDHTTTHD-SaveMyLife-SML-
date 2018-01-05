f04071476a25c1aa305d13aaf8c49bdf0b0ab5c2
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2018-01-05 10:21:28 +0700

    update entity

--- SourceDoAn/SML_QLNPP/DataModel/DistributionManagement.Designer.cs
+++ SourceDoAn/SML_QLNPP/DataModel/DistributionManagement.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\nam4-hk1\hiendai\TH2014-1-PTUDHTTTHD-SaveMyLife-SML\SourceDoAn\SML_QLNPP\DataModel\DistributionManagement.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\TL-HKVII\PTHTTTHD\TH2014-1-PTUDHTTTHD-SaveMyLife-SML\SourceDoAn\SML_QLNPP\DataModel\DistributionManagement.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- SourceDoAn/SML_QLNPP/DataModel/Distributor.cs
+++ SourceDoAn/SML_QLNPP/DataModel/Distributor.cs
@@ -12,7 +12,7 @@ namespace DataModel
     using System;
     using System.Collections.Generic;
     
-    public partial class Distributor : DistributorBase
+    public partial class Distributor
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Distributor()
